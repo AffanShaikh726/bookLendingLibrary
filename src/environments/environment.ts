09d5267e4b73ac0bbe2cda0ad23ca933508e8c59
AffanShaikh726 <affanshaikh0726@gmail.com>
2025-06-08 16:54:21 +0530

    Remove environment.ts from version control

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://sfzgoxmrpynhjlvojnrl.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmemdveG1ycHluaGpsdm9qbnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDI3MTgsImV4cCI6MjA2NDg3ODcxOH0.PgKEuk8M0IeZejV87nPjVTQTIPXqUgvG80qh02Umong'
-};
